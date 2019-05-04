b1bc6672557ffc48ce7ee8b1b1af79fcbe131a8d
suchang <1103232282@qq.com>
2019-05-04 14:38:10 +0800

    irc

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include"irc_user.h"
 #include"irc_server.h"
+#include"irc_request.h"
+#include<iostream>
+
+void test_Request (){
+    std::string t = "NICK username\r\n";
+    auto r = irc::IRCRequest(t);
+    std::cout<<r.cmds[0]<<std::endl;
+};
 
 int main (){
-
+    test_Request();
+    return 0;
 }
