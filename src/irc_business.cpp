25cf38daea40c9118276b4291993f5157a5b7c69
suchang <1103232282@qq.com>
2019-05-04 16:48:44 +0800

    123

--- src/irc_business.cpp
+++ src/irc_business.cpp
@@ -9,16 +9,31 @@ void irc::business::MainLogic(irc::User &user){
 
 void irc::business::Login(irc::User &user){
 
+    auto server = irc::Server::GetInstance();
     auto req = user.IRCRead();
     if(req.op == irc::IRC_REQUEST_OP::NICK) {
+        auto u = server->ReadUser(user.nickName);
+        if (u == NULL ){
+            
+        }
         user.nickName = req.cmds[1];
     }
     // 检查nikc
-
     req = user.IRCRead();
     if (req.op == irc::IRC_REQUEST_OP::USER) {
         user.userName = req.cmds[1];
     }
 
-    user.IRCPushMessage()
+    auto args =  std::vector<std::string>("amy", 
+        "Welcome to the Internet Relay Network borja!borja@polaris.cs.uchicago.edu"
+        );
+
+    auto resp = irc::IRCResponse(
+        server->host,
+        server->port,
+        irc::RESP_CODE::RPL_WELCOME,
+        args 
+    );
+
+    user.IRCPushMessage(resp);
 }
