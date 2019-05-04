798830032b6946fd7314b3cf3c110ff9007d10e6
Chen kai <ck770701780@hotmail.com>
2019-05-04 14:58:08 +0800

    irc user

--- src/irc_session.h
+++ src/irc_session.h
@@ -3,16 +3,22 @@
 #include"irc_error.h"
 #include"irc_response.h"
 #include"irc_request.h"
+#include"irc_user.h"
 
 namespace irc {
-    enum USER_STATE {
+    extern const std::string anonymous;
+    enum USER_SESSION_STATE {
         IDLE,
         P2P,
         CHANNEL
     };
     class UserSession {
     public:
-        USER_STATE state;
+        USER_SESSION_STATE state;
         std::string chatName;
+
+        UserSession():state(IDLE), chatName(anonymous){}
+        UserSession(USER_SESSION_STATE _state, std::string _chatName): state(_state), chatName(_chatName) {};
+        ~UserSession() = default;
     };
 }
--- src/irc_user.h
+++ src/irc_user.h
@@ -1,16 +1,30 @@
 #pragma once
 #include<string>
+#include<queue>
 #include"irc_error.h"
 #include"irc_response.h"
 #include"irc_request.h"
 #include"irc_session.h"
+#include"sclasses.hh"
 
 namespace irc {
+    const std::string anonymous = "*";
     class User {
     public:
+        Socket* socket;
+        std::string nickName;
+        std::string userName;
+        bool state;         //false 离线，true在线
         irc::UserSession session;
+        std::queue<irc::IRCResponse> mesgQueue;
+    public:
+        User();
+        User(std::string _nickName, std::string _userName, Socket *_socket);
+        ~User()=default;
     public:
         irc::ERROR_NO IRCPushMessage(IRCResponse *msg); // 通过套接字给改用户发送消息
         irc::IRCRequest IRCRead(); // 读取请求
+        irc::ERROR_NO Login(Socket *_socket);
+        irc::ERROR_NO Logout();
     };
 }
